6d8d1686cd5b13ccf8ee0dfd785e1f42d5cfb72b
Arman <a@localhost.localdomain>
2018-06-23 20:15:57 -0400

    update link and desc

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(function() 
+{
+  $('a[href^="mailto:"]').each(function() 
+  {
+    this.href = this.href.replace('(at)', '@').replace(/\(dot\)/g, '.');
+    // Remove this line if you don't want to set the email address as link text:
+    this.innerHTML = this.href.replace('mailto:', '');
+  });
+});
+
+
